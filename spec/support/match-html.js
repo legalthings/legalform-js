07d1e01f184acd63e2123cd46c59686c1d620914
Arnold Daniels <arnold@jasny.net>
2017-02-14 21:23:27 +0100

    Initial commit Only has a single very simple test

---
+++ spec/support/match-html.js
@@ -0,0 +1,28 @@
+var compare = require('dom-compare').compare;
+var reporter = require('dom-compare').GroupingReporter;
+var jsdom = require("jsdom");
+
+module.exports = function() {
+  return {
+    compare: function(actual, expected) {
+      if (typeof actual !== 'object') {
+        actual = jsdom.jsdom(actual);
+      }
+
+      if (typeof expected !== 'object') {
+        expected = jsdom.jsdom(expected);
+      }
+
+      var result = compare(expected, actual);
+
+      console.log(result.getResult());
+
+      return {
+        pass: result.getResult(),
+        message: result.getResult()
+          ? "Expected HTML not to match"
+          : "Expected HTML to match: " + reporter.report(result)
+      };
+    }
+  };
+};
