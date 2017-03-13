ddb791fc5124e9ed98fe10054d5af3a5160ad511
Andrew Cherytsya <poratuk@gmail.com>
2017-03-13 16:29:46 +0200

    Changed material style as from document.

--- js/to-material.js
+++ js/to-material.js
@@ -9,8 +9,32 @@ $.fn.toMaterial = function() {
     //Disable input mask on hover for material design
     Inputmask.prototype.defaults.showMaskOnHover = false;
 
+    //Add class to the material design to prevent another styles for it.
+    $docWizard.addClass('material');
+    
     //Move step name on the top and create links for it
     $docWizard.find('h3').each(function(){$(this).prependTo($(this).parent().parent())});
+    
+    //Added prev-next button to the each step
+    var $wizardSteps = $docWizard.find('.wizard-step');
+    var $wizardActions = $('#wizard-prev, #wizard-next, #wizard-done');
+    $wizardSteps.each(function(index, value) {
+        var $wizardForm = $('<div>').appendTo(this);
+            $wizardForm.addClass('wizzard-form');
+            $wizardForm.append($(this).find('form'));
+        var $wizardActions = $('<div>').addClass('wizard-actions').appendTo($wizardForm);
+        console.log($wizardActions); 
+        if (index != 0 ) {
+            $wizardActions.append($('#wizard-prev').clone().removeAttr('id'));
+        }
+        if (index != $wizardSteps.length - 1) {
+            $wizardActions.append($('#wizard-next').clone().removeAttr('id'));
+        }
+        if (index == $wizardSteps.length - 1 ) {
+            $wizardActions.append($('#wizard-done').clone().removeAttr('id'));
+        }
+    });
+    $wizardActions.hide();
 
     /** Change checkboxes to the bootstrap material **/
     $docWizard.find('.form-group .option').each(function(){
@@ -25,7 +49,7 @@ $.fn.toMaterial = function() {
         var $label = $('<label>').appendTo($div);
         $(this).find('input').appendTo($label);
     });
-    console.log('selectize');
+
     //change all selects to the selectize + fix not changed value on select
     $docWizard.find('select').selectize({
       onDropdownClose: function(dropdown) {
