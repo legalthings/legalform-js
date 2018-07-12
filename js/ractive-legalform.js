6cb6b4ec9adfdbade031e07dfa3253863dec6bcd
Sergey Boltonosov <minstel@yandex.ru>
2018-07-12 13:31:17 +0400

    Do not validate selectize fields on page load (#109)

--- js/ractive-legalform.js
+++ js/ractive-legalform.js
@@ -268,14 +268,6 @@
             });
         },
 
-        refreshSelectizes: function () {
-            var ractive = this;
-
-            $('select').each(function() {
-                ractive.validation.handleValidation(this);
-            });
-        },
-
         /**
          * Rebuild the wizard
          */
@@ -312,8 +304,6 @@
             this.refreshLikerts();
 
             metaRecursive(this.meta, $.proxy(this.initField, this));
-
-            setTimeout($.proxy(this.refreshSelectizes, this), 1);
 
             this.on('complete', function() {
                 $('#doc').trigger('shown.preview');
