5cb3e2eeca292b5421519f7ad4d550c73515b6b7
Sergey Boltonosov <minstel@yandex.ru>
2018-11-10 02:36:11 +0700

    Minor fixes

--- js/lib/ltri-to-url.js
+++ js/lib/ltri-to-url.js
@@ -1,45 +1,39 @@
 
-(function($) {
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = ltriToUrl;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = ltriToUrl;
+}
+
+/**
+ * Translate an LTRI to a URL
+ *
+ * @param {string} url  LTRI or URL
+ * @return {string}
+ */
+function ltriToUrl(url) {
+    if (url.match(/^https?:\/\//)) return url;
+
+    var base = document.querySelector('head base').getAttribute('href') || '/';
+    var scheme = window.location.protocol + '//';
+    var host = window.location.host;
+
+    base = base.replace(/service\/[a-z]+\//, 'service/');
+
+    if (!base.match(/^(https?:)?\/\//)) {
+        base = host + '/' + base.replace(/^\//, '');
     }
 
-    if (typeof window !== 'undefined') {
-        window.ltriToUrl = ltriToUrl;
+    url = url.replace('lt:', '');
+    var auth = url.match(/^[^:\/@]+:[^:\/@]+@/);
+    if (auth) {
+        url = url.replace(auth[0], '');
+        base = auth[0] + base;
     }
 
-    /**
-     * Translate an LTRI to a URL
-     *
-     * @param {string} url  LTRI or URL
-     * @return {string}
-     */
-    function ltriToUrl(url) {
-        if (url.match(/^https?:\/\//)) return url;
+    url = url.replace(/^([a-z]+):(\/)?/, function(match, resource) {
+        var start = resource === 'external' ? host : base.replace(/\/$/, '');
 
-        var base = $('head base').attr('href') || '/';
-        var scheme = window.location.protocol + '//';
-        var host = window.location.host;
+        return scheme + start + '/' + resource + '/';
+    });
 
-        base = base.replace(/service\/[a-z]+\//, 'service/');
-
-        if (!base.match(/^(https?:)?\/\//)) {
-            base = host + '/' + base.replace(/^\//, '');
-        }
-
-        url = url.replace('lt:', '');
-        var auth = url.match(/^[^:\/@]+:[^:\/@]+@/);
-        if (auth) {
-            url = url.replace(auth[0], '');
-            base = auth[0] + base;
-        }
-
-        url = url.replace(/^([a-z]+):(\/)?/, function(match, resource) {
-            var start = resource === 'external' ? host : base.replace(/\/$/, '');
-
-            return scheme + start + '/' + resource + '/';
-        });
-
-        return url;
-    }
-})(jQuery);
+    return url;
+}
