17b02aa885a1dde4d174a7057d2fbc380af69a76
Minstel <minstel@yandex.ru>
2019-01-18 18:13:26 +0700

    Rollback casting money to float, as it removes trailing zeros

--- js/legalform-calc.js
+++ js/legalform-calc.js
@@ -2,7 +2,6 @@
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
     module.exports = LegalFormCalc;
     var ltriToUrl = require('./lib/ltri-to-url');
-    var parseNumber = require('./lib/parse-number');
     var expandCondition = require('./lib/expand-condition');
     var calculationVars = require('./lib/calculation-vars');
     var FormModel = require('./model/form-model');
@@ -56,8 +55,6 @@ function LegalFormCalc($) {
 
                     if (type === 'group' && field.multiple) {
                         value = typeof(value) !== 'undefined' ? [value] : [];
-                    } else if (type === 'money' && value) {
-                        value = parseNumber(value);
                     }
 
                     addGroupedData(data, step.group, field.name, value);
